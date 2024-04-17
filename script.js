174cd039fed16d642bed4ef50898790aa4c58cdd
Adnane Ahmimou <ahmimouadnane102@gmail.com>
2024-04-17 12:21:46 +0100

    final-version

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function toggleMenu() {
   var menuToggle = document.querySelector('.toggle');
   var menu = document.querySelector('.menu');
-  menuToggle.classList.toggle('active'); // Corrected here
+  menuToggle.classList.toggle('active');
   menu.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var contactForm = document.getElementById('contact-form');
+  contactForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      emailjs.sendForm('service_ba51ine', 'template_q6b3el5', this)
+          .then(function(response) {
+              console.log('SUCCESS!', response.status, response.text);
+              alert('Your message has been sent successfully!');
+          }, function(error) {
+              console.log('FAILED...', error);
+              alert('Failed to send the message, please try again.');
+          });
+  });
+});
